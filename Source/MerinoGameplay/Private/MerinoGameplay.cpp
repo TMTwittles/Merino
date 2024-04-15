addc178a2e920014b0108d2b7aa8bdf9ba44384b
TristanM <95954032+TMTwittles@users.noreply.github.com>
2024-04-15 10:11:51 +1000

    aiming_camera_system: Created new module for gameplay.

---
+++ Source/MerinoGameplay/Private/MerinoGameplay.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MerinoGameplay.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MerinoGameplay, "MerinoGameplay" );
+ 
