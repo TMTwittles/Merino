13cae31ee9e3dda19e73e8793cc9520371fab240
TristanM <95954032+TMTwittles@users.noreply.github.com>
2024-04-15 10:31:39 +1000

    aiming_camera_system: Linked MerinoGamplay module.

--- Source/Merino.Target.cs
+++ Source/Merino.Target.cs
@@ -10,6 +10,6 @@ public class MerinoTarget : TargetRules
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
-		ExtraModuleNames.Add("Merino");
+		ExtraModuleNames = new List<string>(){"Merino", "MerinoGameplay"};
 	}
 }
